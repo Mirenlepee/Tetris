4dc223ae741fd333555875649e3caf3dcef3dfaa
pemma <pemma@Emma>
2023-10-18 16:38:26 +0200

    m

--- Tetris/src/P1/Ventana_PerfilDeUsuario.java
+++ Tetris/src/P1/Ventana_PerfilDeUsuario.java
@@ -1,5 +1,44 @@
 package P1;
 
-public class Ventana_PerfilDeUsuario {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class Ventana_PerfilDeUsuario extends JFrame {
+  
+       public Ventana_PerfilDeUsuario() {
+        JFrame ventana = new JFrame("Perfil de Usuario");
+        ventana.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        ventana.setSize(400, 300);
+
+        JPanel panel = new JPanel();
+        ventana.setVisible(true);
+       
+
+        
+        JLabel lblNombre = new JLabel("Nombre de Usuario:");
+        JTextField txtNombre = new JTextField(20);
+        JLabel lblBiografia = new JLabel("Biografía:");
+        JTextArea txtBiografia = new JTextArea(5, 20);
+        JButton btnGuardar = new JButton("Continuar");
+
+       
+      
+
+        ventana.add(panel);
+
+       
+        btnGuardar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //CODIFICAR PARA GUARDAR EL NUEVO USUARIO
+            }
+        });
+
+        
+        
+
+        
+    }
 }
--- Tetris/src/P1/Ventana_Registro.java
+++ Tetris/src/P1/Ventana_Registro.java
@@ -1,5 +1,66 @@
 package P1;
 
-public class Ventana_Registro {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class Ventana_Registro extends JFrame{
+    public Ventana_Registro() {
+     
+        JFrame ventana = new JFrame("SIGN UP");
+        ventana.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        ventana.setSize(400, 300);
+        ventana.setVisible(true);
+
+        JPanel panel = new JPanel();
+        ventana.add(panel);
+        panel.setLayout(new BorderLayout());
+
+      
+ 
+   
+
+        JLabel lblTitulo = new JLabel("Sign Up");
+        JLabel lblUsuario = new JLabel("Usuario:");
+        JTextField txtUsuario = new JTextField(20);
+        JLabel lblPassword = new JLabel("Contraseña:");
+        JTextField txtPassword = new JTextField(20);
+        JLabel lblConfirmPassword = new JLabel("Confirmar Contraseña:");
+        JTextField txtConfirmPassword = new JTextField(20);
+        JLabel lblEmail = new JLabel("Correo Electrónico:");
+        JTextField txtEmail = new JTextField(20);
+
+        JButton btnAtras = new JButton("Atrás");
+        JButton btnContinuar = new JButton("Continuar");
+        JButton btnSalir = new JButton("Salir");
+
+    
+  
+        
+        btnAtras.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // CODIFICAR PARA IR A LA VENTANA ANTERIOR
+            }
+        });
+      
+
+        btnContinuar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // CODIFICAR PARA IR A LA SIGUIENTE VENTANA
+            }
+        });
+
+
+        btnSalir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //CODIFICAR PARA IR A LA VENTANA DE CONFIRMACIÓN DE SALIR DEL JUEGO
+            }
+        });
+
+      
+    }
 }

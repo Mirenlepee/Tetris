24292e065bcbc3474d5e6f5eebafc346534fc62a
miren <miren@HP2022>
2023-10-31 11:16:15 +0100

    Base de Ventana Creditos

--- Tetris/src/P1/Ventana_Creditos.java
+++ Tetris/src/P1/Ventana_Creditos.java
@@ -1,10 +1,44 @@
 package P1;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class Ventana_Creditos extends JFrame{
 	private JLabel lblTitulo;
-    private JLabel lblCreadoras;
+    private JLabel lblTexto1;
+    private JLabel lblTexto2;
+    private JLabel lblTexto3;
     private JButton btnVolver;
 
+    public Ventana_Creditos () {
+    	setTitle("Ventana de creditos");
+		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+		setSize( 800, 600 );
+		setLocationRelativeTo( null );
+		setLayout(new BorderLayout());
+    	lblTitulo = new JLabel("CREDITOS");
+    	add(lblTitulo, BorderLayout.NORTH);
+    	lblTexto1 = new JLabel (" Oihane Camacho ");
+    	add(lblTexto1, BorderLayout.NORTH);
+    	lblTexto2 = new JLabel (" Miren Lépée ");
+    	add(lblTexto2, BorderLayout.NORTH);
+    	lblTexto3 = new JLabel (" Naia Lorente ");
+    	add(lblTexto3, BorderLayout.NORTH);
+    	btnVolver = new JButton();
+    	add(btnVolver, BorderLayout.NORTH);
+    	
+    	btnVolver.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+			}
+    		
+    	});
+    }
+    
+    
 }

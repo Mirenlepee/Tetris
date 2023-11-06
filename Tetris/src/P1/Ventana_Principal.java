a0d0b4bca9575c557be0b450a4e907fd30f55a58
miren <miren@HP2022>
2023-11-06 11:15:46 +0100

    Fondo Ventana_Principal

--- Tetris/src/P1/Ventana_Principal.java
+++ Tetris/src/P1/Ventana_Principal.java
@@ -1,24 +1,13 @@
 package P1;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class Ventana_Principal extends JFrame{
 	
@@ -26,52 +15,45 @@ public class Ventana_Principal extends JFrame{
 	
 	public Ventana_Principal() {
 		setTitle("Ventana Principal");
-		setSize(550, 800);
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setLayout(new BorderLayout());
-		setLocationRelativeTo(null);
-		
-		
-/*		try {
-			imgFondo = ImageIO.read(new File("C:\\Users\\Oihane\\git\\Tetris\\Tetris\\src\\P1\\Fondo_VentanaPrincipal.png"));
+        setSize(600, 800);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        try {
+            imgFondo = ImageIO.read(getClass().getResource("Fondo_VentanaPrincipal.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-*/
-		
-		 try {
-	            imgFondo = ImageIO.read(getClass().getResource("Fondo_VentanaPrincipal.png"));
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-		
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        setContentPane(layeredPane);
+
+        // Capa inferior: Imagen de fondo
+        JLabel fondoLabel = new JLabel(new ImageIcon(imgFondo));
+        fondoLabel.setBounds(0, 0, getWidth(), getHeight());
+        layeredPane.add(fondoLabel, JLayeredPane.DEFAULT_LAYER);
+
+        initUI();
+	}
+	
+	
         
-		setContentPane(new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(imgFondo, 0, 0, getWidth(), getHeight(), this);
-            }
-        });
-		
-/*		ImageIcon imgLogotipo = new ImageIcon(Ventana_Principal.class.getResource("Logotipo.png"));
-		JLabel lblLogotipo = new JLabel(imgLogotipo);
-		this.add(lblLogotipo, BorderLayout.NORTH);
-*/
-		
-		ImageIcon imgLogotipo = new ImageIcon(getClass().getResource("Logotipo.png"));
+	private void initUI() {
+		addLogo();
+		addButtons();
+	}
+
+	private void addLogo() {
+        ImageIcon imgLogotipo = new ImageIcon(getClass().getResource("Logotipo.png"));
         JLabel lblLogotipo = new JLabel(imgLogotipo);
-        add(lblLogotipo, BorderLayout.NORTH);
-		
-		JPanel pnlCentro = new JPanel();
-		this.add(pnlCentro, BorderLayout.CENTER);
-		
-		JPanel pnlBotonera = new JPanel();
-		pnlBotonera.setLayout(new BoxLayout(pnlBotonera, BoxLayout.Y_AXIS));
-		// Agrega un espacio fijo vertical entre los botones para centrarlos
-		pnlBotonera.add(Box.createRigidArea(new Dimension(0, 10)));
-		//pnlBotonera.setLayout(new BoxLayout(pnlBotonera, BoxLayout.Y_AXIS));
-		pnlCentro.add(pnlBotonera);
+        lblLogotipo.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lblLogotipo.setBounds(0, 0, getWidth(), imgLogotipo.getIconHeight());
+        lblLogotipo.setOpaque(false); // Hacer que el JLabel sea transparente
+        getContentPane().add(lblLogotipo, JLayeredPane.PALETTE_LAYER); // Establecer un z-index superior
+    }
+	
+	private void addButtons() {
+		JLayeredPane layeredPane = (JLayeredPane) getContentPane();
 		
 		JButton btnPlay = new JButton("PLAY");
 		btnPlay.setFont(new Font("Vendana", Font.BOLD, 16));
@@ -81,13 +63,11 @@ public class Ventana_Principal extends JFrame{
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				new Ventana_SignIn();
-				
 			}
-			
 		});
-		pnlBotonera.add(btnPlay);
-		pnlBotonera.add(Box.createRigidArea(new Dimension(0, 10)));
-		
+		btnPlay.setBounds(100, 200, 200, 30);
+		layeredPane.add(btnPlay, JLayeredPane.PALETTE_LAYER);
+				
 		ImageIcon imgSettings = new ImageIcon("C:\\Users\\miren\\git\\.project\\Tetris\\src\\P1\\Settings.png");
 		JButton btnSettings = new JButton();
 		//Editar el tamaño del logo
@@ -105,9 +85,8 @@ public class Ventana_Principal extends JFrame{
 		        new Ventana_Options();
 		    }
 		});
-
-		pnlBotonera.add(btnSettings);
-		pnlBotonera.add(Box.createRigidArea(new Dimension(0, 10)));
+		btnSettings.setBounds(100, 250, 200, 30);
+		layeredPane.add(btnSettings, JLayeredPane.PALETTE_LAYER);
 		
 		JButton btnStatistics = new JButton("STATISTICS");
 		btnStatistics.setFont(new Font("Vendana", Font.BOLD, 16));
@@ -120,8 +99,8 @@ public class Ventana_Principal extends JFrame{
 			}
 			
 		});
-		pnlBotonera.add(btnStatistics);
-		pnlBotonera.add(Box.createRigidArea(new Dimension(0, 10)));
+		btnStatistics.setBounds(100, 300, 200, 30);
+		layeredPane.add(btnStatistics, JLayeredPane.PALETTE_LAYER);
 		
 		JButton btnCredits = new JButton("CREDITS");
 		btnCredits.setFont(new Font("Vendana", Font.BOLD, 16));
@@ -132,15 +111,16 @@ public class Ventana_Principal extends JFrame{
 				// TODO Auto-generated method stub
 				new Ventana_Creditos();
 			}
-			
 		});
-		
-		pnlBotonera.add(btnCredits);
-		
+		btnCredits.setBounds(100, 350, 200, 30);
+		layeredPane.add(btnCredits, JLayeredPane.PALETTE_LAYER);
+
 	}
+	
 	public static void main(String[] args) {
 		JFrame vent = new Ventana_Principal();
 		vent.setVisible(true);
 		
 	}
+	
 }

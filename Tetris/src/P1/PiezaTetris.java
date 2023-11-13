bc7a2ae598eb53817afd0334a63eb6931ad0abe0
pemma <pemma@Emma.home>
2023-11-13 17:12:05 +0100

    Formas de las piezas

---
+++ Tetris/src/P1/PiezaTetris.java
@@ -0,0 +1,53 @@
+package P1;
+
+
+import java.awt.Color;
+
+public class PiezaTetris {
+
+    private static final int[][][] FORMAS = {
+            //I
+            {
+                {1, 1, 1, 1}
+            },
+            //J
+            {
+                {1, 0, 0},
+                {1, 1, 1}
+            },
+            //L
+            {
+                {0, 0, 1},
+                {1, 1, 1}
+            },
+            //O
+            {
+                {1, 1},
+                {1, 1}
+            },
+            //S
+            {
+                {0, 1, 1},
+                {1, 1, 0}
+            },
+            //T
+            {
+                {0, 1, 0},
+                {1, 1, 1}
+            },
+            //Z
+            {
+                {1, 1, 0},
+                {0, 1, 1}
+            }
+    };
+
+    // Obtener una forma aleatoria
+    public static int[][] obtenerFormaAleatoria() {
+        int ale=(int) (Math.random() * FORMAS.length);
+    	return FORMAS[ale];
+    }
+
+    
+    
+}

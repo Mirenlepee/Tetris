1fc76d4d1b270cd0a131536d57035d50ef325ef1
pemma <pemma@Emma.home>
2023-11-14 13:13:57 +0100

    Ventana Estadísticas

--- Tetris/src/P1/Ventana_Estadistica.java
+++ Tetris/src/P1/Ventana_Estadistica.java
@@ -1,8 +1,11 @@
 package P1;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Ventana_Estadistica extends JFrame{
+public class Ventana_Estadistica extends JFrame {
     private JLabel lblTitle;
     private JLabel lblTotalTime;
     private JLabel lblDailyTime;
@@ -12,11 +15,74 @@ public class Ventana_Estadistica extends JFrame{
     private JLabel lblTotalPoints;
     private JLabel lblDailyAverage;
     private JButton btnBack;
-    
-    public Ventana_Estadistica() {
-    	setSize(600, 800);
-    	setTitle("Estadística");
-    	setVisible(true);
-    }
 
+    public Ventana_Estadistica() {
+        setSize(600, 800);
+        setTitle("Estadística");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(9, 2));
+
+        lblTitle = new JLabel("Estadísticas");
+
+        lblTitle.setFont(new Font("Arial", Font.BOLD, 20));
+        lblTotalTime = new JLabel("Total Time: ");
+        lblDailyTime = new JLabel("Daily Time: ");
+        lblGamesPlayed = new JLabel("Games Played: ");
+        lblMaxPoints = new JLabel("Max Points: ");
+        lblMinPoints = new JLabel("Min Points: ");
+        lblTotalPoints = new JLabel("Total Points: ");
+        lblDailyAverage = new JLabel("Daily Average: ");
+        btnBack = new JButton("Back");
+
+      
+        JTextField txtTotalTime = new JTextField();
+        txtTotalTime.setEditable(false);
+
+        JTextField txtDailyTime = new JTextField();
+        txtDailyTime.setEditable(false);
+
+        JTextField txtGamesPlayed = new JTextField();
+        txtGamesPlayed.setEditable(false);
+
+        JTextField txtMaxPoints = new JTextField();
+        txtMaxPoints.setEditable(false);
+
+        JTextField txtMinPoints = new JTextField();
+        txtMinPoints.setEditable(false);
+
+        JTextField txtTotalPoints = new JTextField();
+        txtTotalPoints.setEditable(false);
+
+        JTextField txtDailyAverage = new JTextField();
+        txtDailyAverage.setEditable(false);
+
+        btnBack.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
+        add(lblTitle);
+        add(new JLabel());
+        add(lblTotalTime);
+        add(txtTotalTime);
+        add(lblDailyTime);
+        add(txtDailyTime);
+        add(lblGamesPlayed);
+        add(txtGamesPlayed);
+        add(lblMaxPoints);
+        add(txtMaxPoints);
+        add(lblMinPoints);
+        add(txtMinPoints);
+        add(lblTotalPoints);
+        add(txtTotalPoints);
+        add(lblDailyAverage);
+        add(txtDailyAverage);
+        add(btnBack);
+        
+        txtTotalTime.setText("Texto de Prueba");
+        
+        setVisible(true);
+    }
 }

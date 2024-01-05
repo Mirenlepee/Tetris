6103c6ddbe980f54b6904f0b0ffe6ea595d58b04
Oihane <Oihane@192.168.1.71>
2024-01-05 15:37:13 +0100

    comentado logger SignIn

--- Tetris/src/ventanas/Ventana_SignIn.java
+++ Tetris/src/ventanas/Ventana_SignIn.java
@@ -115,18 +115,18 @@ public class Ventana_SignIn extends JFrame {
 	
     public Ventana_SignIn() {
     	
-    	super();
-		
-		try {
-            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-        	// logger de prueba
-        	logger.log(Level.SEVERE, "Error al configurar el look and feel", e);
-        	JOptionPane.showMessageDialog(null, "Error al cargar la ventana: contacta con los informaticos.", "ERROR", JOptionPane.ERROR_MESSAGE);
-        }
-
-		// logger de prueba
-        logger.log(Level.INFO, "Inicializando la ventana de inicio");
+//    	super();
+//		
+//		try {
+//            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+//        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+//        	// logger de prueba
+//        	logger.log(Level.SEVERE, "Error al configurar el look and feel", e);
+//        	JOptionPane.showMessageDialog(null, "Error al cargar la ventana: contacta con los informaticos.", "ERROR", JOptionPane.ERROR_MESSAGE);
+//        }
+//
+//		// logger de prueba
+//        logger.log(Level.INFO, "Inicializando la ventana de inicio");
 		
         ventana = new JFrame("Sign In");
         ventana.setSize(400, 250);
